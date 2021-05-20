2bc40dd5248fc8f095a3cb4df0da66136a07f42f
trushka <trueshko@gmail.com>
2021-05-20 07:46:35 +0300

    Rendering bolls to different canvases from one webgl renderer, fix bugs with size calculation

--- js/balls.js
+++ js/balls.js
@@ -13,27 +13,14 @@ export const PI=Math.PI,
 	hLight=new THREE.HemisphereLight('#fff', 0, .3),
 
 	scene=new THREE.Scene(),
-	canvas = document.querySelector('canvas.balls'),
-	cashed=Object.assign({dpr: devicePixelRatio}, canvas.getBoundingClientRect()),
+	canvases = document.querySelectorAll('canvas.balls'),
+	ctx=[],
 
-	renderer = new THREE.WebGLRenderer( {alpha:true, antialias: true, canvas:canvas} ),
+	renderer = new THREE.WebGLRenderer( {alpha:true, antialias: true, canvas: window.OffscreenCanvas && new OffscreenCanvas(1,1)} ),
 	camera=new THREE.PerspectiveCamera( 20, 1, .1, 100 ),
-	camPos0=vec3(0,3,5),
+	camPos0=vec3(0,3,5);
 
-	cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 512, {
-	 format: THREE.RGBAFormat,
-	 generateMipmaps: true,
-	 minFilter: THREE.LinearMipmapLinearFilter,
-	 type: THREE.FloatType,
-	 //encoding: THREE.GammaEncoding
-	} ),
-	cubeCamera = new THREE.CubeCamera( 1, 100000, cubeRenderTarget ),
-	bgScene=new RoomEnvironment();
-
-console.log(renderer.capabilities.isWebGL2);
-
-bgScene.add( cubeCamera );
-cubeCamera.update( renderer, bgScene );
+canvases.forEach(canvas=>{ctx.push(canvas.getContext('2d'))});
 hLight.position.set(1,1,0);
 
 scene.environment = new THREE.PMREMGenerator(renderer).fromScene(new RoomEnvironment(), .05).texture;
@@ -55,33 +42,47 @@ for (var i = 0; i < 3; i++) {
 	satellites.add(ball);
 }
 
-let t0 = performance.now(), t=0, dMax = 80, dMin = 1000/60, camAxis=vec3(1,0,0), y0, bias;
+let t0 = performance.now(), t=0, dMax = 80, dMin = 1000/60, camAxis=vec3(1,0,0), w0=0, bias=[], test=new THREE.Vector4();
 
-renderer.setAnimationLoop(function(){
+//renderer.autoClear = false;
+renderer.setAnimationLoop(function fn(){
 	if (!scene) return;
 	let t1 = performance.now(),
 	 dt = Math.min(t1-t0, dMax);
 	//if (dt<dMin) return; // !Eh || 
 	t0 = t1;
 	t+=dt;
-	if (cashed.dpr!=devicePixelRatio) renderer.setPixelRatio(cashed.dpr=devicePixelRatio);
-	const rect = canvas.getBoundingClientRect();
-	if (rect.top>innerHeight || rect.bottom<0) return
-	if (cashed.width!=rect.width) {
-		renderer.setSize(cashed.w=rect.width, cashed.h=rect.height, false);
-		//camera.updateProjectionMatrix();
-	}
 	satellites.children.forEach(ball=>{
 		ball.position.applyAxisAngle(ball._axis, -dt*.003)
 	})
 	balls.rotateY(-dt*.0005);
-	let bias0=-(rect.top+rect.height-innerHeight/2)/innerHeight;
-	bias=Math.lerp(bias||bias0, bias0, dt*.01)
-	camera.position.copy(camPos0).applyAxisAngle(camAxis, bias);
-	canvas.style.transform=`translateY(${-bias*20}%)`;
-	camera.lookAt(0,0,0);
 	balls.position.y=.1*Math.sin(t*.0017);
-	balls.position.z=-balls.position.y*.4
-	renderer.render(scene, camera)
-	//console.log('r')
+	balls.position.z=-balls.position.y*.4;
+
+	if (renderer.getPixelRatio()!=devicePixelRatio) renderer.setPixelRatio(devicePixelRatio);
+
+	renderer.domElement.transferControlToOffscreen?.();
+	canvases.forEach((canvas, i)=>{
+		const rect = canvas.getBoundingClientRect();
+		if (rect.top>innerHeight || rect.bottom<0) return;
+		if (w0 < rect.width) {
+			renderer.setSize(w0=rect.width, w0, false);
+		}
+			renderer.setViewport(0, w0-rect.width, rect.width, rect.width)
+		if (canvas.width!=renderer.getCurrentViewport(test).z || canvas.height!=test.w) {
+			canvas.width=canvas.height=test.z;
+		}
+
+		let bias0=-(rect.top+rect.height-innerHeight/2)/innerHeight;
+		bias[i]=Math.lerp(bias[i]||bias0, bias0, dt*.01)
+		camera.position.copy(camPos0).applyAxisAngle(camAxis, bias[i]);
+		canvas.style.transform=`translateY(${-bias[i]*20}%)`;
+		camera.lookAt(0,0,0);
+
+		renderer.render(scene, camera);
+
+		ctx[i].clearRect(0, 0, canvas.width, canvas.height);
+		ctx[i].drawImage(renderer.domElement?.transferToImageBitmap() || renderer.domElement, 0, 0)
+	})
 })
+//addEventListener('resize', e=>{w0=0})
